5a0a4d00f98b9a5b7ea7940f6992c9a9ed65dbe9
Huda Khalil <huda_m_khalil@yahoo.com>
2019-06-22 00:11:40 +0200

    Fix - Adding img alt attribute

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
   fetchNeighborhoods();
   fetchCuisines();
 });
-
 /**
  * Fetch all neighborhoods and set their HTML.
  */
@@ -161,6 +160,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `${restaurant.name}`;
   li.append(image);
 
   const name = document.createElement('h1');
